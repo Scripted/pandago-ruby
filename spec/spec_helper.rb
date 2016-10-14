0f5c3d75a2b663fb498a4e61ec8eb3720c7990a2
Tony Ta <tonyta.tt@gmail.com>
2016-10-14 13:05:54 -0700

    make sure PandaGo server is running before running specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,12 @@
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "pandago"
+
 require "pry-byebug"
+
+begin
+  ping = Net::HTTP.new("localhost", 8080).get("/ping")
+rescue Errno::ECONNREFUSED
+  raise "PandaGo server must be running at localhost:8080!"
+end
+
+Pandago.url = "http://localhost:8080"
